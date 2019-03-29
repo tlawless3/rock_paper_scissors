126b9513d0c5c8443a882e41f1e4b375a2aab7b8
Thomas Lawless <tlawless94@gmail.com>
2019-03-29 12:33:49 -0500

    instantiate game component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameComponent } from './game/game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: GameComponent },
+  //example
+  // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  // { path: 'dashboard', component: DashboardComponent },
+  // { path: 'detail/:id', component: HeroDetailComponent },
+  // { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
